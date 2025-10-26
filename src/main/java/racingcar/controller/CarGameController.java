49f87eb5a9110860af2a8d6cf77122d9897b564c
yeseong <yeseong31@naver.com>
2025-10-26 23:23:38 +0900

    refactor(CarGameController, CarGameService): 매개변수에 final 키워드 적용

--- src/main/java/racingcar/controller/CarGameController.java
+++ src/main/java/racingcar/controller/CarGameController.java
@@ -2,8 +2,8 @@ package racingcar.controller;
 
 import racingcar.domain.Cars;
 import racingcar.service.CarGameService;
-import racingcar.view.OutputView;
 import racingcar.view.InputView;
+import racingcar.view.OutputView;
 
 public class CarGameController {
 
@@ -25,12 +25,12 @@ public class CarGameController {
         showWinner(cars);
     }
 
-    private void showWinner(Cars cars) {
+    private void showWinner(final Cars cars) {
         String result = carGameService.receiveWinnerNames(cars);
         outputView.printFinalWinner(result);
     }
 
-    private void executeGame(Cars cars, int attempt) {
+    private void executeGame(final Cars cars, final int attempt) {
         outputView.printGameResult();
 
         for (int iteration = 0; iteration < attempt; iteration++) {
