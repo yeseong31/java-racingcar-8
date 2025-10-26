b2b09d325f8e113472afac48afc6f4cc48427ad5
yeseong <yeseong31@naver.com>
2025-10-26 22:07:55 +0900

    feat(Car): 자동차 전진 횟수 증가 기능 구현

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -5,6 +5,7 @@ import org.junit.platform.commons.util.StringUtils;
 public class Car {
 
     private static final String INVALID_CAR_NAME_MESSAGE = "잘못된 입력입니다.";
+    private static final String FORWARD_TRAIL_INDICATOR = "-";
     private static final int MINIMUM_CAR_NAME_LENGTH = 1;
     private static final int MAXIMUM_CAR_NAME_LENGTH = 5;
 
@@ -19,6 +20,14 @@ public class Car {
         validate(name);
         this.name = name;
         this.forwardCount = forwardCount;
+    }
+
+    public Car increaseForwardCount() {
+        return new Car(name, forwardCount + 1);
+    }
+
+    public String receiveForwardTrail() {
+        return FORWARD_TRAIL_INDICATOR.repeat(forwardCount);
     }
 
     private void validate(String name) {
